bd0eed1b9ec1bb6c3a6b78c963c81f1f9b2a50b6
JosephSpraggon <joe_spraggon@hotmail.co.uk>
2021-01-20 22:42:01 +0000

    Added two more tests

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,7 +1,8 @@
-class Leapyear
+class Leap_year
 
   def leap_year?(year)
-    true
+    year % 400 == 0 ? true : false
+    year % 4 == 0 && year % 100 != 0 ? true : false
   end
 
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,10 +1,10 @@
 require 'leap_year'
 
-describe Leapyear do
-  let(:test) {Leapyear.new}
+describe Leap_year do
+  let(:test) {Leap_year.new}
 
   it 'object of Leapyear class exists' do 
-    expect(test).to be_a_kind_of(Leapyear)
+    expect(test).to be_a_kind_of(Leap_year)
   end
 
   it 'has method leap_year?' do
@@ -14,7 +14,19 @@ describe Leapyear do
   context 'leap_year? tests' do
     
     it 'returns true or false' do
-      expect(test.leap_year?(2000)).to eq true||false
+      expect(test.leap_year?(2000)).to eq true || false
+    end
+
+    it 'All years divisible by 400 ARE leap years' do
+      expect(test.leap_year?(2000)).to eq true
+    end
+
+    it 'All years divisible by 100 but not by 400 are NOT leap years' do
+      expect(test.leap_year?(1700)).to eq false
+    end
+
+    it 'All years divisible by 4 and not by 100 ARE leap years' do
+      expect(test.leap_year?(2004)).to eq true
     end
 
   end
